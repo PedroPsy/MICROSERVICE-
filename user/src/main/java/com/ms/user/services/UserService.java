fb0fd825383e891f4bafc533bfeb927730189c3c
PedroPsy <miguelkarate8@gmail.com>
2025-07-02 11:22:06 -0300

    Iniciando o projeto de estudos

---
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -0,0 +1,28 @@
+package com.ms.user.services;
+
+import com.ms.user.models.UserModel;
+import com.ms.user.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.beans.Transient;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Transactional
+    public UserModel save(@RequestBody @Valid UserModel userModel) {
+        return userRepository.save(userModel);
+    }
+
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+}
