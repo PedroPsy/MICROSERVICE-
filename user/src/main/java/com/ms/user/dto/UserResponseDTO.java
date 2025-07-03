b7b50fd154c917c7ac52b56d6fb35f8b14637441
PedroPsy <miguelkarate8@gmail.com>
2025-07-03 14:40:12 -0300

    Adicionando notações de validação no UserResponseDTO

--- user/src/main/java/com/ms/user/dto/UserResponseDTO.java
+++ user/src/main/java/com/ms/user/dto/UserResponseDTO.java
@@ -1,8 +1,11 @@
 package com.ms.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.UUID;
 
 public record UserResponseDTO (UUID userId,
-                              String name,
-                              String email) {
+                             @NotBlank String name,
+                             @NotBlank @Email String email) {
 }
